388b62f52e6cb4cfb9121e31ef2789f45c9ddfe7
tony <tony.g.kay@gmail.com>
2021-11-30 08:37:30 -0700

    Refactored mac_changer into functions

--- mac_changer.py
+++ mac_changer.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
 import subprocess
+import optparse
 
-sp = subprocess
+def get_arguments():  
+  parser = optparse.OptionParser()
+  parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+  parser.add_option("-m", "--mac", dest="new_mac_address", help="New MAC address (format nn:nn:nn:nn:nn:nn)")
+  return parser.parse_args()
 
-# interface = "eth0"
-# new_mac_address = "00:11:22:33:44:66"
-
-interface = input("Enter interface: ")
-new_mac_address = input("Enter new new MAC in nn:nn:nn:nn:nn:nn format: ")
-
-def mac_change():
+def change_mac(interface, new_mac_address):
   print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
-  sp.call(["ifconfig", interface, "down"])
-  sp.call(["ifconfig", interface, "hw", "ether", new_mac_address])
-  sp.call(["ifconfig", interface, "up"])
+  subprocess.call(["ifconfig", interface, "down"])
+  subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_address])
+  subprocess.call(["ifconfig", interface, "up"])
   print("[+] ifconfig " + interface)
 
 def mac_output():
@@ -24,6 +23,5 @@ def mac_output():
   print("ifconfig " + interface + " up")
   print("[+] ifconfig " + interface)
 
-# mac_change()
-mac_output()
-sp.call(["ifconfig ", interface], shell=True)
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac_address)
