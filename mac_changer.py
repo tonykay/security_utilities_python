c2036895a7b40e8e2561b48c9a37330ebb0cd80e
tony <tony.g.kay@gmail.com>
2021-11-30 09:46:25 -0700

    Added arg checking to mac_cahnger

--- mac_changer.py
+++ mac_changer.py
@@ -7,7 +7,12 @@ def get_arguments():
   parser = optparse.OptionParser()
   parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
   parser.add_option("-m", "--mac", dest="new_mac_address", help="New MAC address (format nn:nn:nn:nn:nn:nn)")
-  return parser.parse_args()
+  (options, arguments) = parser.parse_args()
+  if not options.interface:
+    parser.error("[-] Please specify an interface, use --help for more info.")
+  elif not options.new_mac_address:
+    parser.error("[-] Please specify a new MAC, use --help for more info.")
+  return options
 
 def change_mac(interface, new_mac_address):
   print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
@@ -23,5 +28,5 @@ def mac_output():
   print("ifconfig " + interface + " up")
   print("[+] ifconfig " + interface)
 
-(options, arguments) = get_arguments()
+(options) = get_arguments()
 change_mac(options.interface, options.new_mac_address)
